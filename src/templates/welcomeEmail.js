31a8d888a78f0e1a47ad030fbafe5604aa0874a0
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-02-28 18:09:21 +0530

    update with GUIDLINES_URL argument

--- src/templates/welcomeEmail.js
+++ src/templates/welcomeEmail.js
@@ -1,4 +1,4 @@
-function getEmailContent(WEBSITE, SUPPORT_EMAIL, UNSUB_LINK, receiverName) {
+function getEmailContent(WEBSITE, SUPPORT_EMAIL, UNSUB_URL, GUIDLINES_URL, receiverName) {
 	const welcomeEmailTemplate = `
         <!DOCTYPE html>
 		<html>
@@ -8,12 +8,12 @@ function getEmailContent(WEBSITE, SUPPORT_EMAIL, UNSUB_LINK, receiverName) {
         <body>
             <p>Hi ${receiverName},</p>
             <p>Welcome to iJUJU World, a new experience of shopping along with us to Buy Smart Around you. Now Deals will follow you on the go….Let’s shop smartly.</p>
-            <p>Below are our guidelines as a buyer, please go through once. Also please go through our attachment files for walkthrough features, faq’s of our App.</p>
+            <p>Below are our <a href="${GUIDLINES_URL}">guidelines</a> as a buyer. Please take a moment to go through them. You can also find more information about the features and frequently asked questions in our App.</p>
             <p>Thanks for being with us, we would like to hear from your end with your feedback form to improve our self being a partner– also hope you will enjoy our service in future.</p>
-            <p>Have a great day ahead from Team iJUJU. For more information please visit our WEBSITE <a href="${WEBSITE}">${WEBSITE}</a> or email us on <a href="mailto:${SUPPORT_EMAIL}">${SUPPORT_EMAIL}</a>. Your suggestions are always welcome.</p>
+            <p>Have a great day ahead from Team iJUJU. For more information please visit our WEBSITE <a href="${WEBSITE}">iJUJU</a> or email us on <a href="mailto:${SUPPORT_EMAIL}">${SUPPORT_EMAIL}</a>. Your suggestions are always welcome.</p>
             <p>Thanks &amp; Regards,</p>
             <p>Team iJUJU,</p>
-            <p>If you wish to unsubscribe from our emails, please click <a href="${UNSUB_LINK}">here</a>.</p>
+            <p>If you wish to unsubscribe from our emails, please click <a href="${UNSUB_URL}">here</a>.</p>
         </body>
         </html>`;
 
